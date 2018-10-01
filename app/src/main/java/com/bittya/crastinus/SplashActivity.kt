be0337f9cf867fd84eab2184c8ccb5adc404c709
abhnerAraujo <afga@cin.ufpe.br>
2018-10-01 12:58:05 -0300

    feat: added logo in splash screen fix: action bar hide error

--- app/src/main/java/com/bittya/crastinus/SplashActivity.kt
+++ app/src/main/java/com/bittya/crastinus/SplashActivity.kt
@@ -1,13 +1,24 @@
 package com.bittya.crastinus
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        actionBar.hide()
+        supportActionBar?.hide()
+
+        val imageLogo: Bitmap  = BitmapFactory.decodeResource(resources, R.drawable.ic_logo_white)
+        val resized = Bitmap.createScaledBitmap(imageLogo,
+                kotlin.math.floor(imageLogo.width * 0.7).toInt(),
+                kotlin.math.floor(imageLogo.height * 0.7).toInt(),
+                false)
+
+        logo.setImageBitmap(resized)
     }
 }
